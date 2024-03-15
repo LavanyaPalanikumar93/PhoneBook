b5151f1385181ef62ce19c46ac24d54fe23685d3
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-03-14 21:57:05 -0400

    committed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,12 +1,7 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * Created by leon on 1/23/18.
  * Made WAY better by kristofer 6/16/20
@@ -16,39 +11,49 @@ public class PhoneBook {
     private final Map<String, List<String>> phonebook;
 
     public PhoneBook(Map<String, List<String>> map) {
-        this.phonebook = null;
+        this.phonebook = new LinkedHashMap<>();
     }
 
     public PhoneBook() {
-        this(null);
+        this.phonebook=new LinkedHashMap<>();
     }
 
     public void add(String name, String phoneNumber) {
+        this.phonebook.put(name, Collections.singletonList(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+    this.phonebook.put(name, List.of(phoneNumbers));
+
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
+        phonebook.entrySet();
         return null;
     }
 
     public List<String> lookup(String name) {
+        phonebook.containsKey(name);
         return null;
     }
 
     public String reverseLookup(String phoneNumber)  {
+
         return null;
     }
 
     public List<String> getAllContactNames() {
+        
         return null;
     }
 
     public Map<String, List<String>> getMap() {
+        this.phonebook.get();
+
         return null;
     }
 }
